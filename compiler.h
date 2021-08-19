028b2e67334ea9feacd6dfa6e16e8e6d44f550fb
john cena <gofuck@yourself.com>
2021-08-19 10:23:03 -0400

    Revert "yeah"
    This reverts commit 6ba92da9787f430cc1247e49f0d63a0bcb0c874d.

--- compiler.h
+++ compiler.h
@@ -11,17 +11,7 @@ class Compiler {
 		std::vector<uint8_t> bytecode;
 		std::unordered_map<std::string, FunValue> constants;
 	
-		Compiler(Program* p) : program(p) {}
-		int compile_program();
-
+		Compiler(Program& p) : program(p) {}
 	protected:
-		Program* program;
-};
-
-class FunAssembler {
-	public:
-		FunAssembler() {}
-		uint8_t* fasm(std::string mnem) {
-			return (uint8_t*)NULL;
-		}
+		Program& program;
 };
--- main.cpp
+++ main.cpp
@@ -5,18 +5,14 @@
 #include "parser_tests.h"
 #include "evaluate_tests.h"
 #include "evaluate.h"
-#include "compiler.h"
 #include <memory>
 #include <stdio.h>
 #include <iostream>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
+
 
 // TODO:
 // Long term:
-//	- virtual machine/runtime [FOCUS]
+//	- virtual machine
 //	- bytecode compiler
 //
 // Short term:
@@ -25,6 +21,7 @@
 
 void repl() {
 	std::string input;
+
 	std::cout << "Welcome to funlang!\n";
 	while (1) {
 		std::cout << ">> ";
@@ -34,11 +31,11 @@ void repl() {
 		auto prog = p.parse_program();
 		evaluate_program(prog.get());
 	}
+
 }
 
-//#define RUN_TESTS
-// TODO: some test is corrupting memory or?
-int main(int argc, char** argv) {
+int main() {
+	initialize_maps();
 
 #ifdef RUN_TESTS
 	test_infix_expressions();
@@ -48,30 +45,5 @@ int main(int argc, char** argv) {
 	test_call_expression(false);
 	test_eval_integer();
 #endif
-
-	if (argc < 2) {
-		repl();
-		return 1;
-	}
-
-	char* source_file = argv[1];
-	int sfd = open(source_file, O_RDONLY);
-	if (sfd < 0) {
-		std::cout << "[main] failed to read file " << source_file << '\n';
-		return -1;
-	}
-
-	unsigned long count = lseek(sfd, 0, SEEK_END);
-	lseek(sfd, 0, 0);
-	char* sbuf = (char*)malloc(count + 1);
-	read(sfd, sbuf, count);
-	
-	std::string sinput(sbuf);
-	std::cout << sinput << '\n';
-
-	auto l = Lexer(sinput);
-	auto p = Parser(l);
-	auto prog = p.parse_program();
-	//auto compiler = new Compiler(prog.get()); // UB FTW
-	//compiler->compile_program();
+	repl();
 }
